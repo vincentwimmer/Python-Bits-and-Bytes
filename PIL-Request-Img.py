fcb2cbba5ace399de8fe4930a62a275ed9d96c0c
vincentwimmer <vincentwimmer@gmail.com>
2020-12-21 12:49:03 -0500

    Update PIL-Request-Img.py

--- PIL-Request-Img.py
+++ PIL-Request-Img.py
@@ -2,10 +2,10 @@ from PIL import Image
 import requests 
 from io import BytesIO                                                                      
 
-# Grab the star image from this answer
+# Grab my GitHub profile picture
 response = requests.get('https://avatars1.githubusercontent.com/u/9145304?s=460&u=1126a26b2ffac6187f655816e5eb0ffcb55c429b&v=4')                              
 
-# Make it into a PIL image
+# Convert to PIL
 img = Image.open(BytesIO(response.content))
 
 img.show()
